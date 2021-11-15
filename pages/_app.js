b08f55adf66c308b84425510430f17d825059dc4
Andy Macdonald <hello@andymacdonald.dev>
2021-11-15 09:02:30 +0000

    Social meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import '../styles/index.css'
 import Head from 'next/head'
 import Footer from '../components/footer/Footer'
-import { config } from "@fortawesome/fontawesome-svg-core";
-config.autoAddCss = false;
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
 
 function App ({ Component, pageProps }) {
   return (
@@ -22,7 +22,26 @@ function App ({ Component, pageProps }) {
           rel='stylesheet'
           href='https://use.fontawesome.com/releases/v5.11.2/css/all.css'
         />
-        <title>Third Pig Software Ltd - Software Engineering and Technology Consultancy</title>
+        <title>
+          Third Pig Software Ltd - Software Engineering and Technology
+          Consultancy
+        </title>
+        <meta name='description' content='Experts in the delivery of high quality, high-impact, software
+                solutions.' />
+
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://thirdpigsoftware.com/' />
+        <meta property='og:title' content='Third Pig Software Ltd' />
+        <meta property='og:description' content='Experts in the delivery of high quality, high-impact, software
+                solutions.' />
+
+
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:url' content='https://thirdpigsoftware.com/' />
+        <meta property='twitter:title' content='Third Pig Software Ltd' />
+        <meta property='twitter:description' content='Experts in the delivery of high quality, high-impact, software
+                solutions.' />
+
       </Head>
       <Component {...pageProps} />
       <Footer />
