c7ba2590b62a64b4d9ef6ce576d8fb152f42355f
Andy Macdonald <hello@andymacdonald.dev>
2021-11-30 15:34:42 +0000

    Button fix

--- components/buttons/StandardButton.js
+++ components/buttons/StandardButton.js
@@ -1,6 +1,6 @@
 export const StandardButton = ({ text, onClick }) => (
   <button
-    className='p-4 md:p-10 py-2 ml-2 bg-accent-1 font-bold rounded-2xl border-white buzz-out-on-hover text-white'
+    className='p-4 md:px-10 py-2 ml-2 bg-accent-1 font-bold rounded-2xl border-white buzz-out-on-hover text-white'
     onClick={onClick}
   >
     {text}
