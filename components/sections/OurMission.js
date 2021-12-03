1e4cc545a1ff7c4537a0dab3ea1d43fc1f90b9d2
Andy Macdonald <hello@andymacdonald.dev>
2021-12-03 09:16:03 +0000

    Mission tweaks

--- components/sections/OurMission.js
+++ components/sections/OurMission.js
@@ -6,32 +6,56 @@ export const OurMission = () => {
   return (
     <>
       <div id='mission' className='pt-48 -mt-48 mb-5' />
-      <div className='mt-2 text-black select-none flex flex-row flex-wrap items-start justify-center font-paragraph'>
+      <div className='mt-2 text-black select-none flex flex-row flex-wrap items-center justify-center font-paragraph'>
         <Zoom>
-          <div className='p-10 md:p-0 leading-7 md:text-right text-prose w-full md:w-1/2'>
-            <p className="font-bold text-2xl">Hi I'm Andy 👋, founder of Third Pig Software.</p>
+          <div className='p-10 md:p-0 leading-7 md:text-right text-prose w-full md:w-2/3 md:pr-10'>
+            <p className='font-bold text-2xl'>
+              Hi I'm Andy 👋, founder of Third Pig Software.
+            </p>
             <p className='mt-2'>
               It happens too often:{' '}
               <b>software teams promise a dream, but deliver a nightmare.</b>
             </p>
-            <p className='text-left mt-2'>
-              You handover the reigns of your idea to a team of
-              technical experts, who build and build and build, but miss the
-              mark because they failed to understand your business and the
-              project requirements right at the beginning.
+            <p className='md:text-right mt-2'>
+              You handover the reigns of your idea to a team of technical
+              experts, who build and build and build, but miss the mark because
+              they failed to understand your business and the project
+              requirements right at the beginning.
             </p>
-            <p className='text-left mt-2'>
+            <p className='md:text-right mt-2'>
               I founded Third Pig because I believe things simply don't have to
               be this way.
             </p>
-            <div className='md:px-5 md:py-2 text-left'>
-              <p className='mt-3'>
+          </div>
+        </Zoom>
+        <Fade left>
+          <img
+            style={{ borderWidth: 8 }}
+            src='/static/images/andy.jpeg'
+            className='rounded-full shadow-xl border-white h-32 w-32 mb-10 md:mb-0 md:-mt-2 md:h-48 md:w-48'
+          />
+        </Fade>
+        <div className='flex flex-row flex-wrap items-center justify-center leading-7 md:text-right text-prose'>
+          <Fade left>
+            <div
+              style={{ borderWidth: 8 }}
+              className='bg-gray-200 shadow-xl border-white w-48 h-48 rounded-full flex items-center justify-center'
+            >
+              <img
+                className={`select-none buzz-out-on-hover w-48`}
+                src={'/static/logo.png'}
+              />
+            </div>
+          </Fade>
+          <Fade right>
+            <div className='md:ml-10 px-10 md:px-0 my-5 text-left w-full md:w-2/3'>
+              <p>
                 <b className='text-lg'>
                   We believe in getting to know your business at a deep level
                 </b>{' '}
                 - we want to know how things currently tick, your future goals
-                and your aspirations; then to help you quickly deliver the value you need
-                to achieve them 🚀.
+                and your aspirations; then to help you quickly deliver the value
+                you need to achieve them 🚀.
               </p>
               <p className='mt-3'>
                 <b className='text-lg'>We aren't yes people</b> - we'll
@@ -47,34 +71,18 @@ export const OurMission = () => {
                 that will allow us to invoice more.
               </p>
             </div>
-            <p className='mt-2 text-xl text-left font-semibold'>
+          </Fade>
+        </div>
+        <Fade bottom>
+          <div className='flex flex-col items-center justify-center py-2'>
+            <p className='text-3xl px-10 text-left text-prose font-semibold'>
               Above all else we believe in making our customers successful.
             </p>
-            <p className='mt-2 text-lg text-accent-1 font-bold'>
+            <p className='mt-2 text-lg py-4 text-accent-1 font-bold'>
               - Andy Macdonald, Founder
             </p>
           </div>
-        </Zoom>
-        <div className='md:pl-12 flex md:flex-col justify-center flex-wrap items-center'>
-          <Fade left>
-            <img
-              style={{ borderWidth: 8 }}
-              src='/static/images/andy.jpeg'
-              className='rounded-full shadow-xl border-white md:mt-5 h-32 w-32 md:h-64 md:w-64'
-            />
-          </Fade>
-          <Fade left>
-            <div
-              style={{ borderWidth: 8 }}
-              className='bg-gray-200 shadow-xl border-white md:mt-5 w-48 h-48 md:h-72 md:w-72 rounded-full flex items-center justify-center'
-            >
-              <img
-                className={`select-none buzz-out-on-hover w-48 md:w-72`}
-                src={'/static/logo.png'}
-              />
-            </div>
-          </Fade>
-        </div>
+        </Fade>
       </div>
     </>
   )
