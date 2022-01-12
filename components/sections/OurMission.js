a72f26dea408ea5b9c602d999371ef8b8c8647ed
Andy Macdonald <hello@andymacdonald.dev>
2022-01-12 08:39:23 +0000

    Layout tweaks

--- components/sections/OurMission.js
+++ components/sections/OurMission.js
@@ -54,7 +54,7 @@ export const OurMission = () => {
     <>
       <div id='mission' className='pt-20 -mt-20' />
       <div className='py-12 bg-white'>
-        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+        <div className='max-w-6xl mx-auto px-4 sm:px-6 lg:px-8'>
           <div className='lg:text-center'>
             <h2 className='text-base text-accent-1 font-semibold tracking-wide uppercase'>
               Our Mission
