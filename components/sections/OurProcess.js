21e5a416f56f9141c46bb02f7f032db2d9cbe42b
Andy Macdonald <hello@andymacdonald.dev>
2022-01-12 13:47:50 +0000

    Wording change

--- components/sections/OurProcess.js
+++ components/sections/OurProcess.js
@@ -9,7 +9,7 @@ export const OurProcess = () => (
               Our Process
             </h2>
             <h2 className='text-center mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
-              Find the Value. Deliver it. Repeat. 🔁
+              Spot the Value. Deliver. Repeat. 🔁
             </h2>
             <p className='mt-4 max-w-3xl mx-auto text-center text-xl text-gray-500'>
               Don't bet on a "big bang" launch 💥. We'll take your concept, help
