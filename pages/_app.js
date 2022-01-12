a92808fbe675a005d26d4cc85faf9c6398731afb
Andy Macdonald <hello@andymacdonald.dev>
2022-01-12 08:50:17 +0000

    Fontawesome fix

--- pages/_app.js
+++ pages/_app.js
@@ -44,8 +44,7 @@ function App ({ Component, pageProps }) {
         />
         <link
           rel='stylesheet'
-          href='https://use.fontawesome.com/releases/v5.3.1/css/all.css'
-          integrity='sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU'
+          href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
           crossorigin='anonymous'
         />
         <link rel='icon' type='image/png' href='/static/favicon.ico' />
