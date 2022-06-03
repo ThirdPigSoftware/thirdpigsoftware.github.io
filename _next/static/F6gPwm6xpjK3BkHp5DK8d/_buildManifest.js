bf537ed54c5d6ed8961214213c7f50bb9d614474
AndyMacDroo <hello@andymacdonald.dev>
2022-06-03 19:59:52 +0000

    Deploying to gh-pages from @ ThirdPigSoftware/thirdpigsoftware.github.io@803b5929d35af8fbc374de91b9661a58a702d4ad 🚀

--- _next/static/F6gPwm6xpjK3BkHp5DK8d/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-6dc9603f94e3882f9f03.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-520b06b470f48a8c3284.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
